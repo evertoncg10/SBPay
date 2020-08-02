f949ac8fd8a0c7130f7790b8538b2eb841956cf5
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-08-02 14:26:49 -0300

    Criando um web service de pagamento - Parte 2.

--- src/main/java/br/com/everton/sbpay/service/SBPayService.java
+++ src/main/java/br/com/everton/sbpay/service/SBPayService.java
@@ -13,16 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.everton.sbpay.DadosCartao;
 import br.com.everton.sbpay.PaymentResponse;
+import br.com.everton.sbpay.StatusPagamento;
 
 @RestController
 public class SBPayService {
 
+    private static final String AUT_TOKEN = "r2d2";
+
     @PostMapping(path = "/pay", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PaymentResponse> pagar(@RequestHeader("Token") String token, //
                                                  @Valid @RequestBody DadosCartao dadosCartao, //
-                                                 Errors erros) {
+                                                 Errors errors) {
 
-        return null;
+        if (!AUT_TOKEN.equals(token)) {
+            return ResponseEntity.badRequest().body(new PaymentResponse("Token inválido"));
+        }
 
+        if (errors.hasErrors()) {
+            return ResponseEntity.ok(new PaymentResponse(StatusPagamento.CartaoInvalido));
+        }
+
+        String numCartao = dadosCartao.getNumCartao();
+
+        StatusPagamento status = numCartao.startsWith("111") ? StatusPagamento.Autorizado : StatusPagamento.NaoAutorizado;
+
+        return ResponseEntity.ok(new PaymentResponse(status));
     }
 }
